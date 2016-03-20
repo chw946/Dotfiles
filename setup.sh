2a1e2eb21a5ddf7d6152e47056b1567b0b800f2a
Chen-Hung Wu <chw@mit.edu>
2016-03-20 15:21:54 -0400

    conf vim

--- setup.sh
+++ setup.sh
@@ -8,19 +8,24 @@
 
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="oh-my-zsh vimrc vim tmux.conf"
+rcfiles="vimrc tmux.conf"
 
 # Creating dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "..done"
 
-for file in $files; do
+for file in $rcfiles; do
 	echo "Moving old dotfile to $olddir"
-	mv ~/.$file $olddir
+  mv ~/.$file $olddir	
 	echo "Creating symlink to $file in home dir"
 	ln -s $dir/$file ~/.$file
 done
+
+# Vim setup
+echo "Moving old .vim to $olddir"
+mv ~/.vim $olddir
+cp $dir/vim ~/.vim
 
 # echo "Add some useful"
 chsh -s /bin/zsh
